36fb8bbf253b89b8e87ffa4fa1099a37b301edd8
Mohamed-Tarek-Z <MohamedTarek153@gmail.com>
2025-08-05 01:40:45 +0300

    empty params

--- index.js
+++ index.js
@@ -19,13 +19,23 @@ app.get("/api/hello", (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api", (req, res) => {
+  let yourDate = new Date();
+
+  res.json({ unix: yourDate.getTime(), utc: yourDate.toUTCString() });
+});
+
 app.get("/api/:date", (req, res) => {
+  console.log(`recived ${req.params.date}`);
   const dateParam = req.params.date;
   let yourDate;
   if (!isNaN(dateParam)) {
     yourDate = new Date(parseInt(dateParam));
   } else {
     yourDate = new Date(dateParam);
+  }
+  if (yourDate.toString() === 'Invalid Date') {
+    res.json({ error: 'Invalid Date' });
   }
   res.json({ unix: yourDate.getTime(), utc: yourDate.toUTCString() });
 });
